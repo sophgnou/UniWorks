3c66bb83d6b47cfb2cdd7dc99ee860ff285799d9
sophgnou <sophie.gnou@gmail.com>
2025-04-18 18:33:21 +1000

    Moved updated HTML into PHP file

--- 31748 Programming in the Internet/GroceryStore/index.php
+++ 31748 Programming in the Internet/GroceryStore/index.php
@@ -17,38 +17,185 @@
 <html lang="en">
     <head>
         <title>Grocery Store</title>
-        <meta charset="uts-8" />
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="grocerystore" content="assessment 1" />
         <link rel="stylesheet" href="styles.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     </head>
 
     <body>
-        
-         <header>
-            <img src="#" alt="grocery store logo">
-            <nav class="topnav">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Home</a>
+        <!-- somehow putting the logo and aligning it to the nav bar is really hard, so this is the method that works for me -->
+         <!-- how tf do I do the toggle function -->
+        <header>
+            <nav class="navbar">
+                <a href="index.php" class="logo"><i class="material-icons logo">storefront</i></a>
+                <a href="index.html" class="home active"><i class="material-icons">home</i> Home</a>
+                <a href="about.html" class="about"><i class="material-icons">info</i> About</a>  
+                <div class="dropdown">
+                    <button data-toggle-nav class="dropbtn" onClick="toggleNav()"><i class="material-icons">arrow_drop_down_circle</i> Categories</button>
+                <div class="dropcontent" id="contentDown">
+                        <a href="#">Frozen</a>
+                        <a href="#">Meat</a>
+                        <a href="#">Drinks</a>
+                        <a href="#">Dairy Product</a>
+                        <a href="#">Health Care</a>
+                        <a href="#">Home Supplies</a>
+                        <a href="#">Pet Items</a>
+                </div>
             </nav>
-
-            <div>
-
+            <div class="search-cont">
+                <form class="search-bar" action="action.php">
+                    <input type="text" placeholder="Search products..." name="search">
+                    <button type="submit"><i class="material-icons">search</i></button>
+                    </form>
             </div>
+        </header>
 
-            <form class="search" action="action.php">
-                <input type="text" placeholder="Search..." name="searchbar">
-                <button type="submit">
-                    <i class="fa fa-search"></i>
-                </button>
-            </form>
+        <main>
+        <!-- doing the grid layout gives me the shits, I cannot do this properlyyyyy 16/04/25 -->
+        <!-- I finally figured it out... 17/04/25-->
+            <div class="page-text">
+                <h1>Groceries</h1>
+                <p>Here is what is avaliable.</p>
+            </div>
+            <a href="cart.html">
+                <button id="cart-button"><i class="material-icons">shopping_cart</i></button>
+            </a>
+            <div class="row">
+                <div class="display">
+                    <div class="item">
+                        <img src="images/dog1.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 1</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog2.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 2</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog3.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 3</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog4.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 4</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog5.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 5</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog6.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 6</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog7.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 7</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog8.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 8</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/dog9.jpg" height="125">
+                        <div class="item-content">
+                            <h3>Item 9</h3>
+                            <p>item description</p>
+                            <p class="price">price</p>
+                            <button type="submit" class="add-to-cart">
+                                <p>ADD TO CART <i class="material-icons">add_shopping_cart</i></p>
+                            </button>
+                        </div>
+                    </div>  
+                </div>
+        </main>
 
-            <script>
-
-            </script>
-         </header>
+        <footer>
+            <!-- contact details & uni student deetails -->
+            <h2>Contact</h2>
+            <ul>
+                <li>123 Fake Street, Suburb, State, Country 1234</li>
+                <li>XXXX-XXX-XXX</li>
+                <li><a href="" id="linkedin">Linkedin</a></li>
+            </ul>
+            <p id="copyright"><i class="material-icons link">copyright</i> 2025 Sophie Gnoukhanthone 14241994</p>
+        </footer>
+        <script src="js/togglenav.js"></script>
+        <!--<script type="text/javascript">
+        function toggleNav() {
+        const dropContent = document.getElementById('contentDown');
+        const dropBtn = document.querySelector('[data-toggle-nav]');
         
-    </body>
+        dropContent.classList.toggle('show');
+        dropBtn.classList.toggle('active');
+        }
 
+        // Close when clicking outside
+        document.addEventListener('click', function(event) {
+            if (!event.target.closest('.dropdown')) {
+                const dropContent = document.getElementById('contentDown');
+                const dropBtn = document.querySelector('[data-toggle-nav]');
+                
+                dropContent.classList.remove('show');
+                dropBtn.classList.remove('active');
+            }
+        });
+        </script>-->
+    </body>
 </html>
